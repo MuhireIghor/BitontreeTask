154ce035e17b797db9477b0a3b6c66092f598ab5
MuhireIghor <muhireighor123@gmail.com>
2022-11-05 23:36:04 +0200

    Weather api fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import './App.css';
 import Home from './Pages/Home';
-import {BrowserRouter as Router ,Routes,Route,useNavigate} from 'react-router-dom'
+import {BrowserRouter as Router ,Routes,Route,useNavigate} from 'react-router-dom';
+import { ReactNode } from 'react';
 import Users from './Pages/Users';
 import Weather from './Pages/Weather';
 function App() { 
+
   // const navigate = useNavigate();
 return (
     <div className="App">
       <Router>
         <Routes>
-          {/* <Route path='/' exact element={() => navigate('/add-Users')} /> */}
+          {/* <Route path='/' element={navigate('/add-Users')} /> */}
           <Route path='/Add User' element={<Home />}/>
           <Route path='/Users' element={<Users />}/>
           <Route path='/Weather' element={<Weather />}/>
--- src/Components/WeatherInfo.tsx
+++ src/Components/WeatherInfo.tsx
@@ -2,8 +2,15 @@ import { useEffect } from 'react';
 import axios from 'axios';
 const WeatherInfo = ()=>{
     const API = axios.create({baseURL:"http://api.weatherapi.com/v1"});
-    const fetchUrl = ""
-    // const data = API.get()
+ useEffect(()=>{
+    const location = "kigali";
+    const fetchUrl = `current.json?key=c76ca0b1bf7a41d1bf1202636220511&city name=${location}`;
+    const fetchWeather = async()=>{
+        const {data} =  await API.get(fetchUrl);
+        console.log(data);
+            }
+            fetchWeather();
+ },[fetchUrl])
     useEffect(()=>{
 
     })
