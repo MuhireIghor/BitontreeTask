5e81a70cf68d5a936ac7ab8c56a59079c18a2223
MuhireIghor <muhireighor123@gmail.com>
2022-11-09 09:32:18 +0200

    conditional routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
 import Home from './Pages/Home';
 import {BrowserRouter as Router ,Routes,Route,useNavigate} from 'react-router-dom';
-import { ReactNode } from 'react';
+import { JSXElementConstructor, ReactNode } from 'react';
 import Users from './Pages/Users';
 import Weather from './Pages/Weather';
 function App() { 
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 return (
     <div className="App">
       <Router>
         <Routes>
-          {/* <Route path='/' element={navigate('/add-Users')} /> */}
+          <Route path='/' element={()=>navigate('/Add User')  as  ()=>ReactNode } />
           <Route path='/Add User' element={<Home />}/>
           <Route path='/Users' element={<Users />}/>
           <Route path='/Weather' element={<Weather />}/>
